1b60b45e3304d6c61a4bc2d54e6e193655c0c603
MLucas <manuel.lucas@nology.io>
2020-11-15 21:58:58 +0000

    button function, any input to lowercase and split

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import englishToMorse from './data/data'
 
 const input = document.querySelector("#input");
-const button = document.querySelector("#btn"); 
-const translation = document.querySelector("output");
+const button = document.querySelector("#btn");
+const output = document.querySelector("output");
 
 
-
-
+button.addEventListener("click", () => {
+    let translator = input.value;
+    translator = translator.toLowerCase();
+    translator = translator.split("");
+});
