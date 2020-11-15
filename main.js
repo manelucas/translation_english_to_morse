5736256018082ffe3b0f016c1dc8f6db3f3b43df
MLucas <manuel.lucas@nology.io>
2020-11-15 22:48:00 +0000

    translation from  morse to English functionality

--- main.js
+++ main.js
@@ -27,7 +27,6 @@ let englishToMorse = {
     z: "--..",
 };
 
-
 const input = document.querySelector("#input");
 const button = document.querySelector("#btn");
 const output = document.querySelector("#output");
@@ -43,4 +42,52 @@ button.addEventListener("click", () => {
     translator = translator.join(" ");
     console.log(translator);
     output.innerHTML = translator;
+});
+
+
+
+let morseToEnglish = {
+    ".-": "a", 
+    "-...": "b", 
+    "-.-.": "c", 
+    "-..": "d", 
+    ".": "e", 
+    "..-.":  "f", 
+    "--.": "g", 
+    "....": 'h', 
+    "..":  "i", 
+    ".---": "j", 
+    "-.-": "k", 
+    ".-..": "l", 
+    "--": "m", 
+    "-.": "n", 
+    "---": "o", 
+    ".--.": "p", 
+    "--.-": "q", 
+    ".-.": "r", 
+    "...": "s", 
+    "-": "t", 
+    "..-": "u", 
+    "...-": "v", 
+    ".--": "w", 
+    "-..-": "x", 
+    "-.--": "y", 
+    "--..": "z",
+};
+
+
+const morseInput = document.querySelector("#morseInput");
+const buttonToEnglish = document.querySelector("#btnToEn");
+const outputInEnglish= document.querySelector("#outputInEnglish");
+
+
+buttonToEnglish.addEventListener("click", () => {
+    let translation = morseInput.value;
+    translation = translation.split("");
+    for (var i = 0; i < translation.length; i++) {
+        translation[i] = morseToEnglish[translation[i]];
+    }
+    translation = translation.join(" ");
+    console.log(translation);
+    outputInEnglish.innerHTML = translation;
 });
