9024f35fcf2e784b05627b80d24747998f759b7d
MLucas <manuel.lucas@nology.io>
2020-11-15 22:21:51 +0000

    use of for loop to output translation

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
-import englishToMorse from './data/data'
+let englishToMorse = {
+    a: ".-", 
+    b: "-...", 
+    c: "-.-.", 
+    d: "-..", 
+    e: ".", 
+    f: "..-.", 
+    g: "--.", 
+    h: "....", 
+    i: "..", 
+    j: ".---", 
+    k: "-.-", 
+    l: ".-..", 
+    m: "--", 
+    n: "-.", 
+    o: "---", 
+    p: ".--.", 
+    q: "--.-", 
+    r: ".-.", 
+    s: "...", 
+    t: "-", 
+    u: "..-", 
+    v: "...-", 
+    w: ".--", 
+    x: "-..-", 
+    y: "-.--", 
+    z: "--..",
+};
+
 
 const input = document.querySelector("#input");
 const button = document.querySelector("#btn");
-const output = document.querySelector("output");
+const output = document.querySelector("#output");
 
 
 button.addEventListener("click", () => {
     let translator = input.value;
     translator = translator.toLowerCase();
     translator = translator.split("");
+    for (var i = 0; i < translator.length; i++) {
+        translator[i] = englishToMorse[translator[i]];
+    }
+    translator = translator.join(" ");
+    console.log(translator);
+    output.innerHTML = translator;
 });
